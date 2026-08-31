e92567acaecf993f7056d3f25f7f4ada4c732e8f
Lionel Santoro <lionelsanto2004@gmail.com>
2026-08-30 21:59:24 -0300

    Se crearon las 4 vistas Admin, cajero, inventario y supervisor de caja

---
+++ DesktopAppSupermercado/VistaAdmin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DesktopAppSupermercado
+{
+    public partial class VistaAdmin : Form
+    {
+        public VistaAdmin()
+        {
+            InitializeComponent();
+        }
+    }
+}
